ef6096cc8546e07196bae7ef704db22f8037e0b3
eans25 <eangly.eng@gmail.com>
2018-07-17 23:26:50 +0200

    commit and add readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+THP semaine 3 - exercice Tic Tac Toe
+
+____________peer - programming ____________
+
+GOUACIDE Kwevan
+ENG Eang-Ly
+
+____________     règles de jeu        ____________
+ 
+Jeu sur une grille 3x3
+
+Deux joueurs s'affrontent. Ils doivent remplir chacun à leur tour une case de la grille avec le symbole qui leur est attribué : O ou X. Le gagnant est celui qui arrive à aligner trois symboles identiques, horizontalement, verticalement ou en diagonale.
+
+
---
+++ tic_tac_toe.rb
@@ -0,0 +1,113 @@
+class Player
+  attr_accessor :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+
+  def get_name
+    @name
+  end
+
+  def get_symbol
+    @symbol
+  end
+
+end
+
+class Board
+  attr_accessor :case, :stop_game
+  def initialize
+    @case = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @stop_game = false
+  end
+
+  def draw_board
+    puts '|-----------------|       |-----------------|'
+    puts "|  #{@case[0]}  |  #{@case[1]}  |  #{@case[2]}  |       |  1  |  2  |  3  |"
+    puts '|-----------------|       |-----------------|'
+    puts "|  #{@case[3]}  |  #{@case[4]}  |  #{@case[5]}  |       |  4  |  5  |  6  |"
+    puts '|-----------------|       |-----------------|'
+    puts "|  #{@case[6]}  |  #{@case[7]}  |  #{@case[8]}  |       |  7  |  8  |  9  |"
+    puts '|-----------------|       |-----------------|'
+  end
+
+  def update_board(player)
+    puts "#{player.get_name}, à toi de jouer!!! Entre une valeur de 1 à 9"
+    value = gets.chomp.to_i
+    value -= 1
+
+    if
+      @case[value] != ' '
+      draw_board
+      puts 'Cette case est déja utilisé, entrez une nouvelle valeur pour continuer'
+      # @stop_game = true
+      return false
+    end
+
+    @case[value] = player.get_symbol
+
+    draw_board
+    if win
+      puts "#{player.get_name} à gagné !"
+      @stop_game = true
+    elsif !(@case.any? { |a| a == ' ' })
+      puts 'match nul'
+      @stop_game = true
+    end
+    true
+  end
+
+  def get_stop_game
+    @stop_game
+  end
+
+  # cette fonction est horrible sert à verifier si un des joueur à entré une combinaison gagnante
+  def win
+    if @case[0] != ' ' && @case[0] == @case[1] && @case[0] == @case[2] || @case[3] != ' ' && @case[3] == @case[4] && @case[3] == @case[5] || @case[6] != ' ' && @case[6] == @case[7] && @case[6] == @case[8] || @case[0] != ' ' && @case[0] == @case[3] && @case[0] == @case[6] || @case[1] != ' ' && @case[1] == @case[4] && @case[1] == @case[7] || @case[2] != ' ' && @case[2] == @case[5] && @case[2] == @case[8] || @case[0] != ' ' && @case[0] == @case[4] && @case[0] == @case[8] || @case[2] != ' ' && @case[2] == @case[4] && @case[2] == @case[6]
+
+      true
+    else
+      false
+   end
+  end
+end
+
+
+class Game
+  attr_accessor :player1, :player2
+
+  def initialize
+    puts 'Entrez le nom du 1er joueur:'
+    print '>'
+    @player1 = Player.new(gets.chomp, 'X')
+    puts 'Entrez le nom du 2ème joueur:'
+    print '>'
+    @player2 = Player.new(gets.chomp, 'O')
+  end
+
+  def go
+    board = Board.new
+    board.draw_board
+
+    turn = true
+
+    until board.get_stop_game
+      # break if board.get_stop_game
+
+      if turn == true
+        turn = false
+
+        turn = !turn unless board.update_board(player1)
+      else
+        turn = true
+
+        turn = !turn unless board.update_board(player2)
+      end
+      end
+  end
+end
+
+Game.new.go
+
